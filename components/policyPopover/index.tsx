2c87ed4312a3107496adec3526aed1d5696c954c
Hector Silva-Robles <hectorsilvarobles@Hectors-MacBook-Air.local>
2024-08-09 23:47:59 -0700

    fix

--- components/policyPopover/index.tsx
+++ components/policyPopover/index.tsx
@@ -61,16 +61,16 @@ export default function PolicyPopover() {
           <p>
             <strong>Affiliate</strong> means an entity that controls, is
             controlled by, or is under common control with a party, where
-            "control" means ownership of 50% or more of the shares, equity
-            interest, or other securities entitled to vote for election of
-            directors or other managing authority.
+            &quot;control&quot; means ownership of 50% or more of the shares,
+            equity interest, or other securities entitled to vote for election
+            of directors or other managing authority.
           </p>
         </li>
         <li style={{ marginBottom: "10px" }}>
           <p>
-            <strong>Company</strong> (referred to as either `the Company`, `We"`
-            `Us` or `Our` in this Agreement) refers to ZokunTechnologies, 1713 N
-            114th Ave.
+            <strong>Company</strong> (referred to as either &apos;the
+            Company&apos;, &apos;We&apos;, &apos;Us&apos; or &apos;Our&apos; in
+            this Agreement) refers to ZokunTechnologies, 1713 N 114th Ave.
           </p>
         </li>
         <li style={{ marginBottom: "10px" }}>
@@ -171,7 +171,7 @@ export default function PolicyPopover() {
       <p>Usage Data is collected automatically when using the Service.</p>
 
       <p>
-        Usage Data may include information such as Your Device's Internet
+        Usage Data may include information such as Your Device&apos;s Internet
         Protocol address (e.g. IP address), browser type, browser version, the
         pages of our Service that You visit, the time and date of Your visit,
         the time spent on those pages, unique device identifiers and other
@@ -225,10 +225,10 @@ export default function PolicyPopover() {
       </ul>
 
       <p>
-        Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies
-        remain on Your personal computer or mobile device when You go offline,
-        while Session Cookies are deleted as soon as You close Your web browser.
-        You can learn more about cookies on{" "}
+        Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies.
+        Persistent Cookies remain on Your personal computer or mobile device
+        when You go offline, while Session Cookies are deleted as soon as You
+        close Your web browser. You can learn more about cookies on{" "}
         <a
           href="https://www.termsfeed.com/blog/cookies/#What_Are_Cookies"
           target="_blank"
@@ -326,10 +326,11 @@ export default function PolicyPopover() {
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application's push notifications regarding updates
-            or informative communications related to the functionalities,
-            products or contracted services, including the security updates,
-            when necessary or reasonable for their implementation.
+            such as a mobile application&apos;s push notifications regarding
+            updates or informative communications related to the
+            functionalities, products or contracted services, including the
+            security updates, when necessary or reasonable for their
+            implementation.
           </p>
         </li>
         <li style={{ marginBottom: "10px" }}>
@@ -435,12 +436,13 @@ export default function PolicyPopover() {
       </h3>
 
       <p>
-        Your information, including Personal Data, is processed at the Company's
-        operating offices and in any other places where the parties involved in
-        the processing are located. It means that this information may be
-        transferred to — and maintained on — computers located outside of Your
-        state, province, country or other governmental jurisdiction where the
-        data protection laws may differ than those from Your jurisdiction.
+        Your information, including Personal Data, is processed at the
+        Company&apos;s operating offices and in any other places where the
+        parties involved in the processing are located. It means that this
+        information may be transferred to — and maintained on — computers
+        located outside of Your state, province, country or other governmental
+        jurisdiction where the data protection laws may differ than those from
+        Your jurisdiction.
       </p>
 
       <p>
@@ -545,7 +547,7 @@ export default function PolicyPopover() {
       </p>
 
       <h2 style={{ marginTop: "40px", marginBottom: "20px" }}>
-        Children's Privacy
+        Children&apos;s Privacy
       </h2>
 
       <p>
@@ -561,7 +563,7 @@ export default function PolicyPopover() {
       <p>
         If We need to rely on consent as a legal basis for processing Your
         information and Your country requires consent from a parent, We may
-        require Your parent's consent before We collect and use that
+        require Your parent&apos;s consent before We collect and use that
         information.
       </p>
 
@@ -572,8 +574,8 @@ export default function PolicyPopover() {
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party's site. We strongly advise You to review the Privacy Policy
-        of every site You visit.
+        third party&apos;s site. We strongly advise You to review the Privacy
+        Policy of every site You visit.
       </p>
 
       <p>
@@ -592,8 +594,8 @@ export default function PolicyPopover() {
 
       <p>
         We will let You know via email and/or a prominent notice on Our Service,
-        prior to the change becoming effective and update the "Last updated"
-        date at the top of this Privacy Policy.
+        prior to the change becoming effective and update the &quot;Last
+        updated&quot; date at the top of this Privacy Policy.
       </p>
 
       <p>
